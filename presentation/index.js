fb264c784de8151967df139eb2c19ac504f10cea
amite <amit.erandole@gmail.com>
2017-04-27 16:12:43 +0530

    first draft

--- presentation/index.js
+++ presentation/index.js
@@ -3,15 +3,35 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
+  Code,
+  ComponentPlayground,
   Deck,
+  Fill,
+  Fit,
   Heading,
+  Image,
+  Layout,
+  Link,
   ListItem,
   List,
+  Markdown,
+  MarkdownSlides,
   Quote,
+  S,
   Slide,
-  Text
+  SlideSet,
+  TableBody,
+  TableHeader,
+  TableHeaderItem,
+  TableItem,
+  TableRow,
+  Table,
+  Text,
+  Typeface
 } from "spectacle";
 
 // Import image preloader util
@@ -29,7 +49,13 @@ const images = {
   city: require("../assets/city.jpg"),
   kat: require("../assets/kat.png"),
   logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  markdown: require("../assets/markdown.png"),
+  full_profile: require("../assets/full_user_profile.png"),
+  why: require("../assets/why.gif"),
+  user_widget: require("../assets/user_widget.jpg"),
+  annoyed: require("../assets/annoyed.gif"),
+  widget_query: require("../assets/widget_query.png"),
+  graphql_architechture: require("../assets/graphql_architechture.png"), 
 };
 
 preloader(images);
@@ -40,9 +66,9 @@ const theme = createTheme({
   tertiary: "#03A9FC",
   quartenary: "#CECECE"
 }, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  });
 
 export default class Presentation extends React.Component {
   render() {
@@ -50,28 +76,90 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Intro to GraphQL
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Just the beginner basics + Quick Demo
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            What is GraphQL?
+          </Heading>
+          <Appear>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            What do you think it is?
+          </Text>
+          </Appear>
         </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={5} fit caps lineHeight={1} textColor="secondary" margin="30px auto" >
+            So what's the problem?
+          </Heading>
+          <Appear>
+          <Image src={images.full_profile.replace("/", "")} margin="0px auto 40px" height="650px"/>
+          </Appear>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <CodePane
+            lang="jsx"
+            source={require("raw-loader!../assets/code/user_rest_response.example")}
+            margin="-280px auto"
+          />
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Image src={images.why.replace("/", "")} margin="0px auto 40px" height="550px"/>
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Text margin="40px 0" textColor="primary" size={1} fit bold>
+            Meanwhile..in another place
+          </Text>
+          <Image src={images.user_widget.replace("/", "")} margin="0px auto 40px" height="550px"/>
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Text margin="40px 0" textColor="primary" size={1} fit bold>
+            And now you're wonnderring...
+          </Text>
+          <Image src={images.annoyed.replace("/", "")} margin="0px auto 40px" height="550px"/>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Text margin="40px 0" textColor="primary" textSize={58} bold>
+            but now what if...
+          </Text>
+          <Image src={images.widget_query.replace("/", "")} margin="0px auto 40px" height="600px"/>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>..instead of having multiple “dumb” endpoints, have a single “smart” 
+            endpoint...</Quote>
+            <Cite>Sacha Greif</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary" caps>And that's GraphQL</Heading>
+        </Slide>
+         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+            <Heading size={3} textColor="secondary" caps>Some Problems with REST 😕</Heading>
+            <List>
+            <Appear><ListItem>lots of endpoints</ListItem></Appear>
+            <Appear><ListItem>ambigous endpoints</ListItem></Appear>
+            <Appear><ListItem>weird boundaries btwn client & servers</ListItem></Appear>
+            </List>
+         </Slide>
+         <Slide bgColor="secondary">
+          <Text margin="40px 0" textColor="primary" textSize={44} bold>
+            Where GraphQL sits in your architecture
+          </Text>
+          <Image src={images.graphql_architechture.replace("/", "")} margin="0px auto 40px" height="600px"/>
+         </Slide>
+
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={3} textColor="secondary" caps> 🎸 Why GraphQL Rocks 🙌</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <Appear><ListItem>traverse related objects and their fields</ListItem></Appear>
+            <Appear><ListItem>fetch lots of related data in one request</ListItem></Appear>
+            <Appear><ListItem>avoid making several roundtrips</ListItem></Appear>
+            <Appear><ListItem>Decouple API and database schemas</ListItem></Appear>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
